0fac4dd8e0d749d64782738f1bc3c35774426d59
amanpandey044 <amanpandey331@gmail.com>
2023-09-24 00:16:25 +0530

    update code: automated for log files

--- dist/logAnalyzer.js
+++ dist/logAnalyzer.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const readline = require("readline");
 const fs = require("fs");
 const Table = require('cli-table3');
-const logFilePath = '/Users/nia/Documents/Assignments/log-analysis/prod-api-prod-out.log';
+const logFilePath = process.cwd() + '/';
 const map = {
     "500": "Server Error",
     "404": "Not found",
@@ -15,27 +15,66 @@ const map = {
     "416": "Range Not Satisfiable",
     "422": "Unprocessable Entity"
 };
-const table = new Table({
-    head: ['(index)', 'Status Code', 'Count'],
-});
-const endpointCounts = {};
-const statusCodeCounts = {};
-const readStream = readline.createInterface({
-    input: fs.createReadStream(logFilePath)
-});
-const logLinePattern = /"([A-Z]+) ([^"]+)" (\d+) (\d+|-) "[^"]*" "[^"]*"/;
-readStream.on('line', (line) => {
-    const match = line.match(logLinePattern);
-    if (match) {
-        const endpoint = match[2];
-        const statusCode = match[3];
-        endpointCounts[endpoint] = (endpointCounts[endpoint] || 0) + 1;
-        statusCodeCounts[statusCode] = (statusCodeCounts[statusCode] || 0) + 1;
+function timeDifference(first_line, last_line) {
+    const dateTimePattern = /(\d{4}-\d{2}-\d{2} \d{2}:\d{2} \+\d{2}:\d{2}):/;
+    const match1 = first_line.match(dateTimePattern);
+    const match2 = last_line.match(dateTimePattern);
+    if (match1 && match2) {
+        const date1 = new Date(match1[1]);
+        const date2 = new Date(match2[1]);
+        const timeDifferenceMin = Math.abs((date1.getTime() - date2.getTime()) / (1000 * 60));
+        return timeDifferenceMin;
     }
-});
-readStream.on('close', () => {
-    for (const statusCode in statusCodeCounts) {
-        table.push([map[String(statusCode)], statusCode, statusCodeCounts[statusCode]]);
+    else {
+        return -1;
     }
-    console.log(table.toString());
-});
+}
+async function analyseLog(log_file_name) {
+    let total_api_count = 0, interval_time = 0, first_line = null, last_line = null;
+    const table = new Table({
+        head: ['(index)', 'Status Code', 'Count'],
+    });
+    const endpointCounts = {};
+    const statusCodeCounts = {};
+    const readStream = readline.createInterface({
+        input: fs.createReadStream(logFilePath + log_file_name)
+    });
+    const logLinePattern = /"([A-Z]+) ([^"]+)" (\d+) (\d+|-) "[^"]*" "[^"]*"/;
+    readStream.on('line', (line) => {
+        if (!first_line) {
+            first_line = line;
+        }
+        last_line = line;
+        const match = line.match(logLinePattern);
+        if (match) {
+            const endpoint = match[2];
+            const statusCode = match[3];
+            endpointCounts[endpoint] = (endpointCounts[endpoint] || 0) + 1;
+            statusCodeCounts[statusCode] = (statusCodeCounts[statusCode] || 0) + 1;
+        }
+    });
+    readStream.on('close', () => {
+        let time_interval = timeDifference(first_line, last_line);
+        for (const endpoint in endpointCounts) {
+            table.push([endpoint, endpointCounts[endpoint]]);
+        }
+        for (const statusCode in statusCodeCounts) {
+            total_api_count += statusCodeCounts[statusCode];
+            table.push([map[String(statusCode)], statusCode, statusCodeCounts[statusCode]]);
+        }
+        const api_calls_per_min = total_api_count / time_interval;
+        table.push(['Total Count', "-", total_api_count]);
+        table.push(["Total Count Per Min", "-", api_calls_per_min]);
+        console.log("Log File Name: ", log_file_name);
+        console.log(table.toString());
+    });
+}
+const log_files = ["prod-api-prod-out.log", "api-dev-out.log", "api-prod-out.log"];
+try {
+    log_files.forEach(async function (log) {
+        await analyseLog(log);
+    });
+}
+catch (error) {
+    console.log("error", error);
+}
--- src/logAnalyzer.ts
+++ src/logAnalyzer.ts
@@ -2,7 +2,7 @@ import * as readline from 'readline';
 import * as fs from 'fs';
 const Table = require('cli-table3');
 
-const logFilePath = '/Users/nia/Documents/Assignments/log-analysis/prod-api-prod-out.log';
+const logFilePath = process.cwd()+'/';
 
 const map = {
   "500": "Server Error",
@@ -16,37 +16,95 @@ const map = {
   "422": "Unprocessable Entity"
 }
 
-const table = new Table({
-  head: ['(index)', 'Status Code', 'Count'],
-});
+function timeDifference(first_line: String, last_line: String){
 
-const endpointCounts: { [key: string]: number } = {};
-const statusCodeCounts: { [key: string]: number } = {};
+  const dateTimePattern = /(\d{4}-\d{2}-\d{2} \d{2}:\d{2} \+\d{2}:\d{2}):/;
 
-const readStream = readline.createInterface({
-  input: fs.createReadStream(logFilePath)
-});
+  const match1 = first_line.match(dateTimePattern);
+  const match2 = last_line.match(dateTimePattern);
 
-const logLinePattern = /"([A-Z]+) ([^"]+)" (\d+) (\d+|-) "[^"]*" "[^"]*"/;
-
-readStream.on('line', (line: string) => {
-
-  const match = line.match(logLinePattern);
-  if (match) {
-    const endpoint = match[2];
-    const statusCode = match[3];
-
-    endpointCounts[endpoint] = (endpointCounts[endpoint] || 0) + 1;
-
-    statusCodeCounts[statusCode] = (statusCodeCounts[statusCode] || 0) + 1;
+  if (match1 && match2) {
+    const date1 = new Date(match1[1]);
+    const date2 = new Date(match2[1]);
+    const timeDifferenceMin = Math.abs((date1.getTime() - date2.getTime()) / (1000 * 60));
+    return timeDifferenceMin;
+  } else {
+    return -1;
   }
-});
+}
 
-readStream.on('close', () => {
 
-  for (const statusCode in statusCodeCounts) {
-    table.push([map[String(statusCode)], statusCode, statusCodeCounts[statusCode]]);
-  }
+async function analyseLog(log_file_name: String){
 
-  console.log(table.toString());
-});
+  let total_api_count = 0, interval_time = 0, first_line=null, last_line=null;
+
+  const table = new Table({
+    head: ['(index)', 'Status Code', 'Count'],
+  });
+  
+  const endpointCounts: { [key: string]: number } = {};
+  const statusCodeCounts: { [key: string]: number } = {};
+
+
+  const readStream = readline.createInterface({
+    input: fs.createReadStream(logFilePath+log_file_name)
+  });
+  
+  const logLinePattern = /"([A-Z]+) ([^"]+)" (\d+) (\d+|-) "[^"]*" "[^"]*"/;
+  
+  readStream.on('line', (line: string) => {
+
+    if(!first_line){
+      first_line = line;
+    }
+
+    last_line = line;
+
+    const match = line.match(logLinePattern);
+    if (match) {
+      const endpoint = match[2];
+      const statusCode = match[3];
+      endpointCounts[endpoint] = (endpointCounts[endpoint] || 0) + 1;
+      statusCodeCounts[statusCode] = (statusCodeCounts[statusCode] || 0) + 1;
+    }
+  });
+
+
+  readStream.on('close', () => {
+
+    let time_interval = timeDifference(first_line, last_line)
+
+    for (const endpoint in endpointCounts) {
+      table.push([endpoint, endpointCounts[endpoint]]);
+    }
+  
+    for (const statusCode in statusCodeCounts) {
+      total_api_count += statusCodeCounts[statusCode]
+      table.push([map[String(statusCode)], statusCode, statusCodeCounts[statusCode]]);
+    }
+
+    const api_calls_per_min = total_api_count/time_interval;
+
+    table.push(['Total Count', "-", total_api_count]);
+
+    table.push(["Total Count Per Min", "-", api_calls_per_min]);
+
+    console.log("Log File Name: ", log_file_name)
+
+    console.log(table.toString());
+  });
+  
+}
+
+const log_files = ["prod-api-prod-out.log", "api-dev-out.log", "api-prod-out.log"]
+
+
+try{
+  log_files.forEach(async function (log) {
+    await analyseLog(log)
+  });
+}
+catch(error){
+  console.log("error", error)
+}
+
